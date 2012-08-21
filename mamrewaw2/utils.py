0165fc0002c2bbd02745e05aa6b6113f098db732
Mateusz Fedoryszak <mfedoryszak@gmail.com>
2012-08-21 12:56:28 +0200

    path_info instead path

--- mamrewaw2/utils.py
+++ mamrewaw2/utils.py
@@ -7,7 +7,7 @@ def enrich_and_render_to_response(request, template, context = {}):
     latest_events_list = Event.objects.filter(start_date__gte=datetime.now().date()).order_by('start_date')[:3]
     menu_entries = MenuEntry.objects.order_by('position')
     for entry in menu_entries:
-        entry.check_if_selected(request.path)
+        entry.check_if_selected(request.path_info)
     my_context = {'menu_entries': menu_entries, 'latest_events_list': latest_events_list}
     my_context.update(context)
     return render_to_response(template, my_context)
