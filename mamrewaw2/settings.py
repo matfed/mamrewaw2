26503d22eef7c50c2271079dd9289126bc4eb726
Mateusz Fedoryszak <mfedoryszak@gmail.com>
2012-08-21 12:13:36 +0200

    moving

--- mamrewaw2/settings.py
+++ mamrewaw2/settings.py
@@ -103,7 +103,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'mamrewaw2.urls'
 
 TEMPLATE_DIRS = (
-    'D:/Users/fedo/mamrewaw2_templates'
+    'D:/Users/fedo/mamrewaw2/templates'
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
