bb4446334502ec87d08ed1f22c38a31b96de5304
Mateusz Fedoryszak <mfedoryszak@gmail.com>
2013-10-20 16:51:27 +0200

    minor change

--- mamrewaw2/ballot/management/commands/getballotresults.py
+++ mamrewaw2/ballot/management/commands/getballotresults.py
@@ -6,8 +6,7 @@ from ballot.models import Candidate
 class Command(NoArgsCommand):
     help = "Getting ballot results"
     def handle_noargs(self, **options):
-        #candidates = Vote.objects.annotate(num_votes=Count('candidate')).order_by('num_votes')
-        candidates = Candidate.objects.annotate(num_votes=Count('vote')).order_by('num_votes')
+        candidates = Candidate.objects.annotate(num_votes=Count('vote')).order_by('-num_votes')
         for c in candidates:
             print "{0.name}\t{0.num_votes}".format(c)
 
