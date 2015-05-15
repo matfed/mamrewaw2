8f0832555811b080fe20ef05cd148786fc72943a
Mateusz Fedoryszak <matfed@icm.edu.pl>
2015-05-15 23:09:42 +0200

    custom upload paths

--- mamrewaw2/blog/models.py
+++ mamrewaw2/blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from slugify import slugify
+from os import path
 
 # Create your models here.
 class RecordingSet(models.Model):
@@ -11,19 +13,28 @@ class RecordingSet(models.Model):
     def recordings(self):
         return self.recording_set.all()
 
+
+def recording_content_file_name(instance, filename):
+    fileName, fileExtension = path.splitext(filename)
+    return '/'.join(['blog', 'recordings', instance.set.date.strftime('%Y%m%d'), slugify(instance.title)[:64] + fileExtension])
+
 class Recording(models.Model):
     title = models.CharField(max_length=200)
-    content = models.FileField(upload_to='blog/recordings')
+    content = models.FileField(upload_to=recording_content_file_name)
     type = models.CharField(max_length=200)
     set = models.ForeignKey(RecordingSet)
 
     def __unicode__(self):
         return self.title
 
+def post_img_file_name(instance, filename):
+    fileName, fileExtension = path.splitext(filename)
+    return '/'.join(['blog', 'photos', str(instance.pk), 'main' + fileExtension])
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateTimeField()
-    img = models.ImageField(upload_to='blog/photos', blank=True, null=True)
+    img = models.ImageField(upload_to=post_img_file_name, blank=True, null=True)
     lead = models.CharField(max_length=2000)
     text = models.TextField()
     recordingSet = models.ForeignKey(RecordingSet, blank=True, null=True)
--- mamrewaw2/blog/tests.py
+++ mamrewaw2/blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from models import *
+from datetime import datetime
 
 # Create your tests here.
+class FileNamesTest(TestCase):
+    def test_recording_content_file_name(self):
+        set = RecordingSet.objects.create(title="set", date=datetime.strptime("2015-03-01", "%Y-%m-%d"))
+        recording = Recording.objects.create(title="title", content="content", type="type", set=set)
+        self.assertEqual(recording_content_file_name(recording, 'name.mp3'), 'blog/recordings/20150301/title.mp3')
+
+    def test_recording_content_file_name(self):
+        post = Post.objects.create(
+            title="title", 
+            date=datetime.strptime("2015-03-01", "%Y-%m-%d"),
+            img="img",
+            lead="lead",
+            text="text")
+
+        self.assertEqual(post_img_file_name(post, 'name.jpg'), 'blog/photos/1/main.jpg')
